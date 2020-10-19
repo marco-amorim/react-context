78a68a376242641f38e6689eedba63fbe07aab21
marco-amorim <marcoamorim95@hotmail.com>
2020-10-19 10:35:38 -0300

    onclick event to change language state

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+	state = { language: 'english' };
+
+	onLanguageChange = (language) => {
+		this.setState({ language });
+	};
+
 	render() {
-		return <div className="ui container">App</div>;
+		return (
+			<div className="ui container">
+				<div>
+					Select a Language:
+					<i
+						className="flag us"
+						onClick={() => this.onLanguageChange('english')}
+					/>
+					<i
+						className="flag nl"
+						onClick={() => this.onLanguageChange('dutch')}
+					/>
+					{this.state.language}
+				</div>
+			</div>
+		);
 	}
 }
 
