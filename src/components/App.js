2720035079e8cac415c68c85a80e86c54dfa65d0
marco-amorim <marcoamorim95@hotmail.com>
2020-10-22 20:57:34 -0300

    passing state to context through Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends Component {
 	state = { language: 'english' };
@@ -21,7 +22,9 @@ class App extends Component {
 						className="flag nl"
 						onClick={() => this.onLanguageChange('dutch')}
 					/>
-					<UserCreate />
+					<LanguageContext.Provider value={this.state.language}>
+						<UserCreate />
+					</LanguageContext.Provider>
 				</div>
 			</div>
 		);
