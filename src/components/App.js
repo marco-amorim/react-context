3ecedd0baf77ef1653f082ff689b89c2d0e4e6f4
marco-amorim <marcoamorim95@hotmail.com>
2020-10-22 21:19:41 -0300

    pulling from multiple contexts with Consumer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends Component {
 	state = { language: 'english' };
@@ -22,9 +23,11 @@ class App extends Component {
 						className="flag nl"
 						onClick={() => this.onLanguageChange('dutch')}
 					/>
-					<LanguageContext.Provider value={this.state.language}>
-						<UserCreate />
-					</LanguageContext.Provider>
+					<ColorContext.Provider value="red">
+						<LanguageContext.Provider value={this.state.language}>
+							<UserCreate />
+						</LanguageContext.Provider>
+					</ColorContext.Provider>
 				</div>
 			</div>
 		);
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends Component {
-	render() {
+	renderButton(color) {
 		return (
-			<button className="ui button primary">
+			<button className={`ui button ${color}`}>
 				<LanguageContext.Consumer>
 					{(value) => (value === 'english' ? 'Submit' : 'Voorleggen')}
 				</LanguageContext.Consumer>
 			</button>
+		);
+	}
+
+	render() {
+		return (
+			<ColorContext.Consumer>
+				{(color) => this.renderButton(color)}
+			</ColorContext.Consumer>
 		);
 	}
 }
