9344efa93f919e39c8df5f73ca1b4b27f7ab282b
marco-amorim <marcoamorim95@hotmail.com>
2020-10-22 21:12:56 -0300

    using Consumer instead of this.context to retrieve Context value

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
-	static contextType = LanguageContext;
-
 	render() {
-		const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-		return <button className="ui button primary">{text}</button>;
+		return (
+			<button className="ui button primary">
+				<LanguageContext.Consumer>
+					{(value) => (value === 'english' ? 'Submit' : 'Voorleggen')}
+				</LanguageContext.Consumer>
+			</button>
+		);
 	}
 }
 
