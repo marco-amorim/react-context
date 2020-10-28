a2a74e88395dc00a6b53835e5bcb83e8aa225385
marco-amorim <marcoamorim95@hotmail.com>
2020-10-28 13:10:54 -0300

    extracting language selector logic to another component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends Component {
 	state = { language: 'english' };
@@ -13,22 +14,12 @@ class App extends Component {
 	render() {
 		return (
 			<div className="ui container">
-				<div>
-					Select a Language:
-					<i
-						className="flag us"
-						onClick={() => this.onLanguageChange('english')}
-					/>
-					<i
-						className="flag nl"
-						onClick={() => this.onLanguageChange('dutch')}
-					/>
-					<ColorContext.Provider value="red">
-						<LanguageContext.Provider value={this.state.language}>
-							<UserCreate />
-						</LanguageContext.Provider>
-					</ColorContext.Provider>
-				</div>
+				<LanguageSelector onLanguageChange={this.onLanguageChange} />
+				<ColorContext.Provider value="red">
+					<LanguageContext.Provider value={this.state.language}>
+						<UserCreate />
+					</LanguageContext.Provider>
+				</ColorContext.Provider>
 			</div>
 		);
 	}
